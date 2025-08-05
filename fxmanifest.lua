41f236946d5cbb076d3dd8b20d2a17bd5e984f6d
xVespex <clementebambam@gmail.com>
2025-08-05 15:55:17 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'Optimized 2-Step & Antilag Script with Toggle and Custom Sound Loader'
+version '1.1.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css',
+    'sounds/twostep_custom.ogg',
+    'sounds/backfire_custom.ogg',
+    'sounds/turboflutter_custom.ogg'
+}
